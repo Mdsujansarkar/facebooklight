70a8c786714d246ca5cf2e5ae5173b370dee28d3
Mdsujansarkar <sujansarkar786@gmail.com>
2018-01-22 22:54:15 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+/*
+this file is empty
+
+*/
+
---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name:facebook light box
+Plugin URL:facebook.com
+Author:sujan
+Author URL:sujansarkar.com
+Description:first 
+*/
+
